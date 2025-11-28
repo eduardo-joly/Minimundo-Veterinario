a48b732b7045dd2cc968f53c5cfed0105780d432
eduardo-joly <102835211+eduardo-joly@users.noreply.github.com>
2025-11-28 12:58:32 -0300

    Add SQL updates for Cliente, Pet, and Veterinario

---
+++ 04_updates.sql
@@ -0,0 +1,5 @@
+Atualizações (UPDATE)
+
+UPDATE Cliente SET telefone = '11988887777' WHERE id_cliente = 1;
+UPDATE Pet SET raca = 'Labrador Retriever' WHERE id_pet = 1;
+UPDATE Veterinario SET especialidade = 'Dermatologia' WHERE id_veterinario = 2;
